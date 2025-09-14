5871d6704e1b28c3c516b6b02f583a86c75b7558
NonsensePluto <kirill210804@mail.ru>
2025-09-14 15:23:57 +0300

    Добавлены модули data и domain, описан план со всеми действиями, замечаниями и вопросами по проекту

---
+++ data/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.coursesapp"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+
+    //Domain
+    implementation(project(":domain"))
+
+    // Retrofit
+    implementation(libs.retrofit2)
+    implementation(libs.retrofit2GsonConverter)
+
+    //OkHttp
+    implementation(libs.okhttp3)
+    implementation(libs.okhttp3Logging)
+
+    //Coroutines
+    implementation(libs.coroutinesCore)
+    implementation(libs.coroutinesAndroid)
+
+    //Coroutine Lifecycle Scopes
+    implementation(libs.lifecycleViewModel)
+
+    //Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    //kapt(libs.androidx.room.compiler)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "Courses App"
 include(":app")
- 
+include(":data")
+include(":domain")
