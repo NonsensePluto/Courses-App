d79615e8bfc70d3deba9dc63b968bfe445796ee3
NonsensePluto <kirill210804@mail.ru>
2025-09-15 16:36:31 +0300

    Организовал работу DI через Koin

--- data/src/main/java/com/example/coursesapp/data/repository/LoginRepositoryImpl.kt
+++ data/src/main/java/com/example/coursesapp/data/repository/LoginRepositoryImpl.kt
@@ -4,14 +4,11 @@ import com.example.coursesapp.domain.repository.LoginRepository
 
 class LoginRepositoryImpl() : LoginRepository {
     override fun login(email: String, password: String) {
-        TODO("Not yet implemented")
     }
 
     override fun loginWithVk() {
-        TODO("Not yet implemented")
     }
 
     override fun loginWithOk() {
-        TODO("Not yet implemented")
     }
 }
