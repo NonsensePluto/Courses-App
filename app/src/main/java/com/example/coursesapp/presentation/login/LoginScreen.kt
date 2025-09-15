e7194c926f78efb7f16b2ae648a4495747f3334b
NonsensePluto <kirill210804@mail.ru>
2025-09-15 14:02:28 +0300

    Подставил макет входа в приложение

---
+++ app/src/main/java/com/example/coursesapp/presentation/login/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.example.coursesapp.presentation.login
+
+import androidx.fragment.app.Fragment
+import com.example.coursesapp.R
+
+class LoginScreen() : Fragment(R.layout.login_screen) {
+
+}
